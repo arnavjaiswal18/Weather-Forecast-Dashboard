c313aafeb8d66f482e5da21dacdec770fea13a3c
Aranav Jaiswal <arnavjai18@gmail.com>
2024-10-27 18:46:08 +0530

    Plot the Graph(temperature vs date) for random input. Use 'plotly.express', Data Visualization Library, to plot the graph.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -8,3 +8,15 @@ days = st.slider("Forecast Days ", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+
+def get_data(days):
+    dates = ["2024-10-28", "2024-10-29", "2024-10-30"]
+    temperatures = [13, 12, 15]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+
+# d, t = get_data(days)  # Put d and t equal to x and y respectively
+figure = px.line(x=get_data(days)[0], y=get_data(days)[1], labels={"x": "Date", "y": "Temperature(C)"})
+st.plotly_chart(figure)
